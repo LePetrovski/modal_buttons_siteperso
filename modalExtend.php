695d24388854263c739a32606799d2997e19e302
LePetrovski <pierre.alberge@gmail.com>
2018-07-31 16:14:23 +0200

    Ajouté de Tooltips dans la class modal_body de la version étendue

--- modalExtend.php
+++ modalExtend.php
@@ -15,7 +15,10 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+
+        <h5>Tooltips in a modal</h5>
+  	<p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
+
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
